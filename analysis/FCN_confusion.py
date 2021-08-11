88a064a2923c2244500b465e828aa3af3789d6e3
Jimmy Tabet <jstabet@email.unc.edu>
2021-08-11 18:56:58 -0400

    added viz for FCN results

--- analysis/FCN_confusion.py
+++ analysis/FCN_confusion.py
@@ -96,7 +96,20 @@ for count, file in enumerate(files):
 
     res = heatmodel.predict(raw_FCN)
   
-    pro = res[:,:,:,3].squeeze()
+    # NEW STUFF--------
+    bord = 4
+    res_new = tf.transpose(res, perm=[0, 3, 1, 2]).numpy()
+    res = []
+    for i in res_new.squeeze():
+        res.append(cv2.copyMakeBorder(i, bord, bord, bord, bord, 0))
+        
+    res = np.array(res)
+    res = res[np.newaxis,...]
+    # NEW STUFF--------
+    
+  
+    # pro = res[:,:,:,3].squeeze()
+    pro = res[:,3,:,:].squeeze()
   
     # if pro.max()>0.5:
     #   pass
@@ -118,9 +131,9 @@ for count, file in enumerate(files):
   
     top_left = cv2.minMaxLoc(pro)[-1]
     sh_y, sh_x = top_left
-  
     
-    if sh_y == 0 or sh_y == pro.shape[0]-1 or sh_x == 0 or sh_x == pro.shape[0]-1:
+    # if sh_y == 0 or sh_y == pro.shape[0]-1 or sh_x == 0 or sh_x == pro.shape[0]-1:
+    if sh_y == 0+bord or sh_y == pro.shape[0]-bord-1 or sh_x == 0+bord or sh_x == pro.shape[0]-bord-1:
     # if sh_y == pro.shape[0]-1 or sh_x == pro.shape[0]-1:
       # continue
       sh_y_n = sh_y
@@ -137,7 +150,8 @@ for count, file in enumerate(files):
       sh_x_n = (sh_x - ms_h + (log_xm1_y - log_xp1_y)/(2 * log_xm1_y - four_log_xy + 2 * log_xp1_y))
       sh_y_n = (sh_y - ms_w + (log_x_ym1 - log_x_yp1)/ (2 * log_x_ym1 - four_log_xy + 2 * log_x_yp1))
     
-    factor = raw_FCN.shape[1]/pro.shape[0]
+    # factor = raw_FCN.shape[1]/pro.shape[0]
+    factor = raw_FCN.shape[1]/(pro.shape[0]-bord)
     FCN_x = factor*sh_y_n
     FCN_y = factor*sh_x_n
     
@@ -174,7 +188,7 @@ center_dist = np.array(center_dist)
 dist_thresh = 100
 TPR_all = []
 FPR_all = []
-for pro_thresh in [0.25]:#np.linspace(0,1,1001):
+for pro_thresh in np.linspace(0,1,1001):
     # pro_thresh = .25
     
     FCN_pro = FCN_max > pro_thresh
@@ -186,7 +200,7 @@ for pro_thresh in [0.25]:#np.linspace(0,1,1001):
     fn = 0
     tp = 0
     tn = 0
-    oor = 0
+    # oor = 0
     
     for FCN, gt, dis in zip(FCN_pro, gt_pro, center_dist):
         if gt == 0:
@@ -201,14 +215,16 @@ for pro_thresh in [0.25]:#np.linspace(0,1,1001):
             elif dis < dist_thresh:
                 tp += 1
             else:
-                oor += 1
+                # oor += 1
+                fp += 1
+                fn += 1
                 
     print('thresh:', pro_thresh)
     print('tp:', tp)
     print('tn:', tn)
     print('fp:', fp)
     print('fn:', fn)
-    print('OOR:', oor)
+    # print('OOR:', oor)
     
     TPR_all.append(tp/(tp+fn))
     FPR_all.append(fp/(fp+tn))
@@ -232,9 +248,11 @@ plt.ylabel('TPR')
 plt.title(f'ROC, AUC = {metrics.auc(FPR_all, TPR_all).round(3)}')
 
 #%% visualize results
+plt.close('all')
+
 
 label = ['blurry','edge','interphase','prophase','unique']
-pro_col = label.index('interphase')
+pro_col = label.index('prophase')
 
 pro_thresh = 0.7
 
@@ -242,12 +260,13 @@ completed = 0
 
 num_to_show = 1
 
-num_to_break = 10
+num_to_break = 8
 
 for count, file in enumerate(files):
+    is_bord=False
     
-    if count % 100 == 0:
-        print(f'{count} of {len(files)}')
+    # if count % 100 == 0:
+    #     print(f'{count} of {len(files)}')
     
     # only use files that were not part of training set
     if not int(datetime.datetime.fromtimestamp(os.path.getmtime(file)).strftime('%m%d'))>617:
@@ -270,8 +289,17 @@ for count, file in enumerate(files):
     raw_FCN = raw_FCN[np.newaxis,...,np.newaxis]
 
     res = heatmodel.predict(raw_FCN)
+
+    bord = 4
+    res_new = tf.transpose(res, perm=[0, 3, 1, 2]).numpy()
+    res = []
+    for i in res_new.squeeze():
+        res.append(cv2.copyMakeBorder(i, bord, bord, bord, bord, 0))
+        
+    res = np.array(res)
+    res = res[np.newaxis,...]
   
-    pro = res[:,:,:,pro_col].squeeze()
+    pro = res[:,pro_col,:,:].squeeze()
   
     if pro.max()>0.7:
         pass
@@ -285,60 +313,69 @@ for count, file in enumerate(files):
     ms_w = 0#np.ceil(pro.shape[0]/2)
   
     # pro = cv2.resize(pro,(514,514))
-    pro = cv2.copyMakeBorder(pro,4,4,4,4,0)
+    # pro = cv2.copyMakeBorder(pro,bord,bord,bord,bord,0)
     top_left = cv2.minMaxLoc(pro)[-1]
     sh_y, sh_x = top_left
   
-    if sh_y == 0 or sh_y == pro.shape[0]-1 or sh_x == 0 or sh_x == pro.shape[0]-1:
+    if sh_y == 0+bord or sh_y == pro.shape[0]-bord-1 or sh_x == 0+bord or sh_x == pro.shape[0]-bord-1:
     # if sh_y == pro.shape[0]-1 or sh_x == pro.shape[0]-1:
       # continue
       sh_y_n = sh_y
       sh_x_n = sh_x
-    
+      is_bord = True
+      
     else:
       # peak registration
-      log_xm1_y = np.log(pro[sh_x - 1, sh_y])
-      log_xp1_y = np.log(pro[sh_x + 1, sh_y])
-      log_x_ym1 = np.log(pro[sh_x, sh_y - 1])
-      log_x_yp1 = np.log(pro[sh_x, sh_y + 1])
-      four_log_xy = 4 * np.log(pro[sh_x, sh_y])
-  
-      sh_x_n = (sh_x - ms_h + (log_xm1_y - log_xp1_y)/(2 * log_xm1_y - four_log_xy + 2 * log_xp1_y))
-      sh_y_n = (sh_y - ms_w + (log_x_ym1 - log_x_yp1)/ (2 * log_x_ym1 - four_log_xy + 2 * log_x_yp1))
+        log_xm1_y = np.log(pro[sh_x - 1, sh_y])
+        log_xp1_y = np.log(pro[sh_x + 1, sh_y])
+        log_x_ym1 = np.log(pro[sh_x, sh_y - 1])
+        log_x_yp1 = np.log(pro[sh_x, sh_y + 1])
+        four_log_xy = 4 * np.log(pro[sh_x, sh_y])
     
-    factor = raw_FCN.shape[1]/(pro.shape[0]-4)
-    FCN_x = factor*sh_y_n
-    FCN_y = factor*sh_x_n
-
-    fig2 = plt.figure()#figsize=(20,20))
-
-    ax = fig2.add_subplot(num_to_show,6,6*(num_to_show-1)+1)
-    ax.imshow(raw.squeeze(),cmap='gray')
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.set_title('original')
-    ax.scatter(factor*sh_y_n, factor*sh_x_n, c='b', label='MC')
-    max_loc = np.unravel_index(pro.argmax(), pro.shape)
-    ax.scatter(factor*max_loc[1], factor*max_loc[0], c='r', label='OG')
-    # ax.legend()
-      
-    from matplotlib.patches import Rectangle
-    ax.add_patch(Rectangle((factor*sh_y_n-286/2, factor*sh_x_n-286/2), 286, 286, fill=False, edgecolor='g'))
-    
-    res = tf.transpose(res, perm=[0, 3, 1, 2]).numpy()
-    for num, i in enumerate(res.squeeze()):
-      ax = fig2.add_subplot(num_to_show,6,6*6*(num_to_show-1)+2+num)
-      ax.imshow(i, cmap='gray')
-      ax.set_xticks([])
-      ax.set_yticks([])
-      ax.set_title(label[num]+' heatmap')
-      if num == pro_col:
-        max_loc = np.unravel_index(i.argmax(), i.shape)
-        ax.scatter(sh_y_n, sh_x_n, c='b', label='MC')
-        ax.scatter(*max_loc[::-1], c='r', label='OG')
-        # ax.legend()
+        sh_x_n = (sh_x - ms_h + (log_xm1_y - log_xp1_y)/(2 * log_xm1_y - four_log_xy + 2 * log_xp1_y))
+        sh_y_n = (sh_y - ms_w + (log_x_ym1 - log_x_yp1)/ (2 * log_x_ym1 - four_log_xy + 2 * log_x_yp1))
         
+    factor = raw_FCN.shape[1]/(pro.shape[0]-bord)
+
     completed += 1
+    # if completed == num_to_break:
+    if True:
+        fig2 = plt.figure()#figsize=(20,20))
     
-    if completed > num_to_break:
-        break
+        ax = fig2.add_subplot(num_to_show,6,6*(num_to_show-1)+1)
+        ax.imshow(raw.squeeze(),cmap='gray')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_title('original')
+        ax.scatter(factor*sh_y_n, factor*sh_x_n, c='b', label='MC')
+        # max_loc = np.unravel_index(pro.argmax(), pro.shape)
+        ax.scatter(factor*sh_y, factor*sh_x, c='r', label='OG')
+        # ax.legend()
+          
+        from matplotlib.patches import Rectangle
+        ax.add_patch(Rectangle((factor*sh_y_n-286/2, factor*sh_x_n-286/2), 286, 286, fill=False, edgecolor='g'))
+        
+        # res = tf.transpose(res, perm=[0, 3, 1, 2]).numpy()
+        for num, i in enumerate(res.squeeze()):
+          ax = fig2.add_subplot(num_to_show,6,6*6*(num_to_show-1)+2+num)
+          ax.imshow(i, cmap='gray')
+          ax.set_xticks([])
+          ax.set_yticks([])
+          ax.set_title(label[num]+' heatmap')
+          if num == pro_col:
+            # max_loc = np.unravel_index(i.argmax(), i.shape)
+            ax.scatter(sh_y_n, sh_x_n, c='b', label='MC')
+            ax.scatter(sh_y, sh_x, c='r', label='OG')
+            # ax.legend()
+        
+        if is_bord: print('border')
+        print((sh_y_n, sh_x_n))
+        print(sh_y, sh_x)
+        print()
+        
+        # break
+    
+
+    
+    # if completed >= num_to_break:
+    #     break
