9acd823c7a7c580e89bc0566fe6889ba4c4dff98
Jimmy Tabet <jstabet@email.unc.edu>
2021-08-12 19:03:09 -0400

    added manual sliding window batches, better peak localization

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ For GPU performance with tensorflow: cudatoolkit and cuda dnn are needed
 Installation on windows has been a bit troublesome.  For example making sure that the python environment is reading the correct cuda runtime dlls (e.g. cudnn64_7.dll) has been a problem.  You may need to search the computer and remove other versions of cudnn64_7.dll
 
 ```
-conda create -n sm_gpu tensorflow-estimator=2.0.0 tensorflow-gpu=2.0.0 scikit-image matplotlib opencv spyder
+conda create -n sm_gpu tensorflow-estimator=2.0.0 tensorflow-gpu=2.0.0 h5py=2.10.0 scikit-image matplotlib opencv spyder
 conda activate  sm_gpu
 pip3 install torch==1.9.0+cu102 torchvision==0.10.0+cu102 torchaudio===0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip3 install cellpose
@@ -45,4 +45,8 @@ Score threshold for having the microscope go take a high resolution 3D image of:
 
 # FCN Install instructions
 
-'''connda create -n sm_FCN tensorflow-gpu=2 spyder numpy opencv matplotlib'''
+'''conda create -n sm_FCN tensorflow-estimator=2.0.0 tensorflow-gpu=2.0.0 h5py=2.10.0 spyder numpy opencv matplotlib scikit-learn'''
+
+# Annotator Install instructions
+
+'''conda create -n sm_annotator tensorflow-estimator=2.0.0 tensorflow-gpu=2.0.0 h5py=2.10.0 spyder opencv scipy'''
--- analysis/FCN_confusion.py
+++ analysis/FCN_confusion.py
@@ -56,11 +56,12 @@ def get_conv(input_shape=(286, 286, 1), filename=None):
 # half_size = input_size[0]//2
 
 tile_path = '/home/nel-lab/NEL-LAB Dropbox/NEL/Datasets/smart_micro/Cellpose_tiles/annotation_results'
-files = sorted(glob.glob(os.path.join(tile_path, '**','*.npz'), recursive=True))
-files = [fil for fil in files if not 'og_backup' in fil]
+# files = sorted(glob.glob(os.path.join(tile_path, '**','*.npz'), recursive=True))
+# files = [fil for fil in files if not 'og_backup' in fil]
 
 # nn_path = '/home/nel-lab/Desktop/Jimmy/Smart Micro/FCN/2021-06-16-18-58-19.214988.h5'
-nn_path = '/home/nel-lab/Desktop/Jimmy/Smart Micro/FCN/2021-08-04-15-57-31.199175_edge.h5'
+nn_path = '/home/nel/Desktop/Smart Micro/FCN/2021-08-04-15-57-31.199175_edge.h5'
+
 heatmodel = get_conv(input_shape=(None, None, 1), filename=nn_path)
 
 #%% loop
@@ -247,6 +248,12 @@ plt.xlabel('FPR')
 plt.ylabel('TPR')
 plt.title(f'ROC, AUC = {metrics.auc(FPR_all, TPR_all).round(3)}')
 
+#%% raw tif files
+import glob,os
+path = '/home/nel/Desktop/Smart Micro/ShannonEntropy_2Dimgs'
+files = sorted(glob.glob(os.path.join(path, '**','*.tif'), recursive=True))
+files = [fil for fil in files if not 'annotated_examples' in fil]
+
 #%% visualize results
 plt.close('all')
 
@@ -269,20 +276,22 @@ for count, file in enumerate(files):
     #     print(f'{count} of {len(files)}')
     
     # only use files that were not part of training set
-    if not int(datetime.datetime.fromtimestamp(os.path.getmtime(file)).strftime('%m%d'))>617:
-        continue
+    # if not int(datetime.datetime.fromtimestamp(os.path.getmtime(file)).strftime('%m%d'))>617:
+    #     continue
         
-    dat = np.load(file, allow_pickle=True)
+    # dat = np.load(file, allow_pickle=True)
     
-    raw = dat['raw'].copy()
+    # raw = dat['raw'].copy()
     # raw = cv2.copyMakeBorder(raw,143,143,143,143,0)
-    mask = dat['masks']
-    labels = dat['labels']
-    labels_dict = dat['labels_dict'].item()
+    # mask = dat['masks']
+    # labels = dat['labels']
+    # labels_dict = dat['labels_dict'].item()
     
-    stages = [labels_dict[lab] for lab in labels]
+    # stages = [labels_dict[lab] for lab in labels]
     
-    pro_id = np.where(np.array(stages) == 'prophase')[0]+1
+    # pro_id = np.where(np.array(stages) == 'prophase')[0]+1
+    
+    raw = plt.imread(file)
     
     raw_FCN = raw - raw.mean()
     raw_FCN /= raw.std()
@@ -294,13 +303,196 @@ for count, file in enumerate(files):
     res_new = tf.transpose(res, perm=[0, 3, 1, 2]).numpy()
     res = []
     for i in res_new.squeeze():
-        res.append(cv2.copyMakeBorder(i, bord, bord, bord, bord, 0))
+        res.append(cv2.copyMakeBorder(i, bord, bord, bord, 0, 0))
         
     res = np.array(res)
     res = res[np.newaxis,...]
   
     pro = res[:,pro_col,:,:].squeeze()
   
+    if pro.max()>0.7:
+        pass
+    # elif len(pro_id) > 0:
+    #     pass
+        # print('missed one!', file)
+    else:
+        continue
+    
+    ms_h = 0#np.ceil(pro.shape[0]/2)
+    ms_w = 0#np.ceil(pro.shape[0]/2)
+  
+    # pro = cv2.resize(pro,(514,514))
+    # pro = cv2.copyMakeBorder(pro,bord,bord,bord,bord,0)
+    top_left = cv2.minMaxLoc(pro)[-1]
+    sh_y, sh_x = top_left
+  
+    if sh_y == 0+bord or sh_y == pro.shape[0]-bord-1 or sh_x == 0+bord or sh_x == pro.shape[0]-bord-1:
+    # if sh_y == pro.shape[0]-1 or sh_x == pro.shape[0]-1:
+      # continue
+      sh_y_n = sh_y
+      sh_x_n = sh_x
+      is_bord = True
+      
+    else:
+      # peak registration
+        log_xm1_y = np.log(pro[sh_x - 1, sh_y])
+        log_xp1_y = np.log(pro[sh_x + 1, sh_y])
+        log_x_ym1 = np.log(pro[sh_x, sh_y - 1])
+        log_x_yp1 = np.log(pro[sh_x, sh_y + 1])
+        four_log_xy = 4 * np.log(pro[sh_x, sh_y])
+    
+        sh_x_n = (sh_x - ms_h + (log_xm1_y - log_xp1_y)/(2 * log_xm1_y - four_log_xy + 2 * log_xp1_y))
+        sh_y_n = (sh_y - ms_w + (log_x_ym1 - log_x_yp1)/ (2 * log_x_ym1 - four_log_xy + 2 * log_x_yp1))
+        
+    factor = raw_FCN.shape[1]/(pro.shape[0]-bord)
+
+    completed += 1
+    # if completed == num_to_break:
+    if True:
+        fig2 = plt.figure()#figsize=(20,20))
+    
+        ax = fig2.add_subplot(num_to_show,6,6*(num_to_show-1)+1)
+        ax.imshow(raw.squeeze(),cmap='gray')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_title('original')
+        ax.scatter(factor*sh_y_n, factor*sh_x_n, c='b', label='MC')
+        # max_loc = np.unravel_index(pro.argmax(), pro.shape)
+        ax.scatter(factor*sh_y, factor*sh_x, c='r', label='OG')
+        # ax.legend()
+          
+        from matplotlib.patches import Rectangle
+        ax.add_patch(Rectangle((factor*sh_y_n-286/2, factor*sh_x_n-286/2), 286, 286, fill=False, edgecolor='g'))
+        
+        # res = tf.transpose(res, perm=[0, 3, 1, 2]).numpy()
+        for num, i in enumerate(res.squeeze()):
+          ax = fig2.add_subplot(num_to_show,6,6*6*(num_to_show-1)+2+num)
+          ax.imshow(i, cmap='gray')
+          ax.set_xticks([])
+          ax.set_yticks([])
+          ax.set_title(label[num]+' heatmap')
+          if num == pro_col:
+            # max_loc = np.unravel_index(i.argmax(), i.shape)
+            ax.scatter(sh_y_n, sh_x_n, c='b', label='MC')
+            ax.scatter(sh_y, sh_x, c='r', label='OG')
+            # ax.legend()
+        
+        if is_bord: print('border')
+        print((sh_y_n, sh_x_n))
+        print(sh_y, sh_x)
+        print()
+        
+        break
+    
+
+    
+    if completed >= num_to_break:
+        break
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%% visualize results - NEW
+plt.close('all')
+
+
+label = ['blurry','edge','interphase','prophase','unique']
+pro_col = label.index('prophase')
+
+pro_thresh = 0.7
+
+completed = 0
+
+num_to_show = 1
+
+num_to_break = 8
+
+for count, file in enumerate(files):
+    is_bord=False
+    
+    if count % 100 == 0:
+        print(f'{count} of {len(files)}')
+    
+    # only use files that were not part of training set
+    if not int(datetime.datetime.fromtimestamp(os.path.getmtime(file)).strftime('%m%d'))>617:
+        continue
+        
+    dat = np.load(file, allow_pickle=True)
+    
+    raw = dat['raw'].copy()
+    raw = cv2.copyMakeBorder(raw,143,143,143,143,0)
+    mask = dat['masks']
+    labels = dat['labels']
+    labels_dict = dat['labels_dict'].item()
+    
+    stages = [labels_dict[lab] for lab in labels]
+    
+    pro_id = np.where(np.array(stages) == 'prophase')[0]+1
+    
+    # raw = plt.imread(file)
+    
+    raw_FCN = raw - raw.mean()
+    raw_FCN /= raw.std()
+    raw_FCN = raw_FCN[np.newaxis,...,np.newaxis]
+
+    # add border, batch sliding window
+    raw_FCN_bigger = cv2.copyMakeBorder(raw_FCN.squeeze(), 143, 143, 143, 143, 0)
+    raw_FCN_bigger = raw_FCN_bigger[np.newaxis, ..., np.newaxis]
+    print(raw_FCN_bigger.shape)
+    
+    batch=[]
+    for i1,i in enumerate(range(143,raw_FCN_bigger.shape[1]-143,16)):
+        for j1,j in enumerate(range(143, raw_FCN_bigger.shape[1]-143, 16)):
+            print((i1,j1))
+            batch.append(raw_FCN_bigger[:1,i-143:i+143,j-143:j+143,:1])
+    batch=np.concatenate(batch, axis=0)
+    
+    res = heatmodel.predict(batch)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    res = heatmodel.predict(raw_FCN)
+
+    
     if pro.max()>0.7:
         pass
     elif len(pro_id) > 0:
@@ -373,9 +565,54 @@ for count, file in enumerate(files):
         print(sh_y, sh_x)
         print()
         
-        # break
+        break
     
 
     
-    # if completed >= num_to_break:
-    #     break
+    if completed >= num_to_break:
+        break
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%% pass sliding window in batches
+raw_FCN_bigger = cv2.copyMakeBorder(raw_FCN.squeeze(), 143, 143, 143, 143, 0)
+raw_FCN_bigger = raw_FCN_bigger[np.newaxis, ..., np.newaxis]
+print(raw_FCN_bigger.shape)
+
+batch=[]
+for i1,i in enumerate(range(143,raw_FCN_bigger.shape[1]-143,16)):
+    for j1,j in enumerate(range(143, raw_FCN_bigger.shape[1]-143, 16)):
+        print((i1,j1))
+        batch.append(raw_FCN_bigger[:1,i-143:i+143,j-143:j+143,:1])
+batch=np.concatenate(batch, axis=0)
+
+res = heatmodel.predict(batch)
+
+#%% plot results
+plt.subplot(1,6,1)
+plt.imshow(raw, cmap='gray')
+plt.title('raw')
+plt.axis('off')
+
+for i in range(5):
+    plt.subplot(1,6,i+2)
+    plt.imshow(res[:,:,:,i].reshape(50,50), cmap='gray')
+    plt.title(label[i]+' heatmap')
+    plt.axis('off')
+
+#%%
+for i in range(5):
+    plt.subplot(1,6,i+1)
+    plt.imshow(cv2.copyMakeBorder(np.reshape(res[:,:,:,i],(33,33)), 16, 16, 16, 16, 0), cmap='gray')
+plt.subplot(1,6,6);plt.imshow(raw, cmap='gray')
